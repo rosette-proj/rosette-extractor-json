4a296755fb5946a6ace53918cea80dab04dc833c
Matt Low <mlow@lumoslabs.com>
2014-12-15 11:01:24 -0800

    Add JSON extractor

---
+++ lib/rosette/extractors/json-extractor.rb
@@ -0,0 +1,52 @@
+# encoding: UTF-8
+
+require 'json/stream'
+require 'rosette/core'
+
+module Rosette
+  module Extractors
+
+    class JsonExtractor < Rosette::Core::StaticExtractor
+
+      def extract_each_from(json_content)
+        if block_given?
+          parse(json_content) do |key_str, value_str|
+            yield make_phrase(value_str, key_str)
+          end
+        else
+          to_enum(__method__, json_content)
+        end
+      end
+
+      def supports_line_numbers?
+        false
+      end
+
+      protected
+
+      def parse(json_content, &blk)
+        parser = JSON::Stream::Parser.new do
+          key(&:key)
+          value { |value_str| value(value_str, blk) }
+        end
+
+        parser << json_content
+      end
+
+      class KeyValueExtractor < JsonExtractor
+        protected
+
+        def key(key_str)
+          @key = key_str
+        end
+
+        def value(value_str)
+          if block_given?
+            yield @key, value_str
+          end
+        end
+      end
+
+    end
+  end
+end

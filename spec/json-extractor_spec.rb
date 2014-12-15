c7e7b2d7c5bc00f3f16e1661b4cdb4d1948691a1
Matt Low <mlow@lumoslabs.com>
2014-12-15 12:02:28 -0800

    Add basic spec with fixtures

---
+++ spec/json-extractor_spec.rb
@@ -0,0 +1,32 @@
+# encoding: UTF-8
+
+require 'spec_helper'
+
+include Rosette::Extractors
+
+describe JsonExtractor do
+  FIXTURE_MANIFEST.each_pair do |extractor_name, expected_results|
+    describe extractor_name do
+      let(:extractor) do
+        capitalized_name = Rosette::Core::StringUtils.camelize(extractor_name.to_s)
+        JsonExtractor.const_get("#{capitalized_name}Extractor").new
+      end
+
+      expected_results.each_pair do |expected_file, expected_phrases|
+        it "extracts phrases correctly from #{expected_file}" do
+          source_file = File.join(FIXTURE_DIR, expected_file)
+
+          extractor.extract_each_from(File.read(source_file)).each do |actual_phrase|
+            expected_phrase_index = expected_phrases.find_index { |phrase| phrase['meta_key'] == actual_phrase.meta_key }
+            expected_phrase = expected_phrases[expected_phrase_index]
+            expect(expected_phrase).to_not be_nil
+            expect(expected_phrase['key']).to eq(actual_phrase.key)
+            expected_phrases.delete_at(expected_phrase_index)
+          end
+
+          expect(expected_phrases).to be_empty
+        end
+      end
+    end
+  end
+end
